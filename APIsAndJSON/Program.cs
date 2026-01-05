9e60ff339a0b8a4b46bf14ae2502930eb1e765f9
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2026-01-05 15:46:54 -0500

    x1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace APIsAndJSON
-{
+﻿    using System.Text.Json.Nodes; 
+    using Newtonsoft.Json.Linq;
+    using APIsAndJSON;
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+
+            Console.WriteLine("Kanye is upset, and says to Ron, :");
+            
+            APIsAndJSON.RonVSKanyeAPI.KanyeQuote();
+            
+            Console.WriteLine("Ron walks out of the bathroom and says back, :");
+            
+            APIsAndJSON.RonVSKanyeAPI.RonQuote();
+            
+            Console.WriteLine("Kanye, with tears in his eyes says, :");
+            
+            APIsAndJSON.RonVSKanyeAPI.KanyeQuote();
+            
+            APIsAndJSON.RonVSKanyeAPI.RonQuote();
+            
+            Console.WriteLine("Finally, Kanye says, :");
+            
+            APIsAndJSON.RonVSKanyeAPI.KanyeQuote();
+            
+            APIsAndJSON.RonVSKanyeAPI.RonQuote();
+            
+            APIsAndJSON.RonVSKanyeAPI.KanyeQuote();
+            
         }
     }
-}
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeUrl = "https://api.kanye.rest/";
+            var kanyeResponse = client.GetStringAsync(kanyeUrl).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote")!.ToString();
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Kanye: '{kanyeQuote}'");
+            Console.WriteLine("---------------------");
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+            var ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronUrl).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace("[", "").Replace("]", "");
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Ron: '{ronQuote}'");
+            Console.WriteLine("---------------------");
+        }
     }
 }
+
